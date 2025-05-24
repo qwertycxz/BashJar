bb4d1791f0b749c9fbc1ee00a5b4866e7a6b17fd
qwertycxz <gmail2@qq.com>
2025-05-24 09:58:44 +0800

    style: indent

--- Bash.java
+++ Bash.java
@@ -1,6 +1,6 @@
 import java.io.IOException;
 public class Bash {
 	public static void main(String[] args) throws InterruptedException, IOException {
-        new ProcessBuilder("bash").inheritIO().start().waitFor();
+		new ProcessBuilder("bash").inheritIO().start().waitFor();
 	}
 }
