d30262eab353a39f4174a07a7aadd1381bfd9bae
qwertycxz <gmail2@qq.com>
2025-05-29 13:50:14 +0800

    Add pm2-resurrect before bash

--- Bash.java
+++ Bash.java
@@ -1,6 +1,10 @@
 import java.io.IOException;
 public class Bash {
 	public static void main(String[] args) throws InterruptedException, IOException {
+		var pm2 = new ProcessBuilder("pm2", "resurrect");
+		var environment = pm2.environment();
+		environment.put("PATH", environment.get("PATH") + ":/home/container/bin:/home/container/.bun/bin");
+		pm2.start();
 		new ProcessBuilder("bash").inheritIO().start().waitFor();
 	}
 }
